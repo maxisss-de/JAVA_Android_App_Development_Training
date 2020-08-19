48a0cad20efc3beeb35bd7a98a7ea89c7b2a8eb4
MaximFeoktistov <69854595+MaximFeoktistov@users.noreply.github.com>
2020-08-19 03:47:33 +0300

    Create Main.java

---
+++ task_1.6/Main.java
@@ -0,0 +1,18 @@
+/*
+Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
+*/
+
+
+/*
+Ответ
+*/
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите значение = ");
+        double a = scanner.nextDouble();
+        double cashback = a+ a*0.15;
+        System.out.println("Вы получили "+cashback);
+    }
+}
