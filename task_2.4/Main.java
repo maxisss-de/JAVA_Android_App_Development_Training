cf919b3f8d549dabb863ad726d63c9cc2a9e907f
MaximFeoktistov <69854595+MaximFeoktistov@users.noreply.github.com>
2020-08-25 04:01:32 +0300

    Update Main.java

--- task_2.4/Main.java
+++ task_2.4/Main.java
@@ -34,3 +34,16 @@ public class Main {
 
     }
 }
+
+
+/* или так: ответ 2 */
+
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int number = 1; number <= 5; number++) {
+            sum += number;
+            System.out.println(sum);
+        }
+    }
+}
