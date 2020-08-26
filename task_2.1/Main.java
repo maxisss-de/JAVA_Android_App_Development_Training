e15d59c824f61b12d079807f4c6a80c986ad0505
MaximFeoktistov <69854595+MaximFeoktistov@users.noreply.github.com>
2020-08-26 14:03:27 +0300

    Update Main.java

--- task_2.1/Main.java
+++ task_2.1/Main.java
@@ -11,15 +11,15 @@
 /* Ответ*/ 
 public class Main {
     public static int min(int a, int b, int c) {
-       if (a<b) {
-           if (b>c) {return c;}
-           else if (a==b) {
-               if (a>c) {return c;}
-               else {return a;}
-           }
-           else {return b;}
-       }
-       else {return a;}
+        if (a>b) { 
+            if (b>c) return c;
+            else if (a==b) {
+                if (a>c) return c;
+                else return a;
+            }
+            else return b;
+        }
+        else return a;
     }
     public static void main(String[] args) {
         System.out.println(min(1, 2, 3));
