96af7a77c6d67e1830bfb38aa1b67c265a637a45
MaximFeoktistov <69854595+MaximFeoktistov@users.noreply.github.com>
2020-08-27 03:46:18 +0300

    Update Main.java

--- task_2.6/Main.java
+++ task_2.6/Main.java
@@ -5,12 +5,12 @@
 4 8 12 16 ...
 ... */
 
-/* Ответ */
+/* Ответ (не доделал)*/
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i < 10; i++) {
-            for (int j = 1; j < 10; j++) {
+        for (int i = 1; i < 11; i++) {
+            for (int j = 1; j < 11; j++) {
                 System.out.println(i*j);
             }
         }
