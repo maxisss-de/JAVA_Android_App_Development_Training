6dfa1d6502689440b99371b50ad58a7c408a2fe8
MaximFeoktistov <69854595+MaximFeoktistov@users.noreply.github.com>
2020-09-02 00:51:25 +0300

    Create Main.java

---
+++ task_5.1/Main.java
@@ -0,0 +1,48 @@
+/*
+Перед решением прочтите https://www.codeflow.site/ru/article/java-buffered-reader
+Задача: Программа определяет, какая семья (фамилию) живёт в доме с указанным номером.
+Новая задача: Программа должна работать не с номерами домов, а с городами:
+Пример ввода:
+Москва
+Ивановы
+Киев
+Петровы
+Лондон
+Абрамовичи
+Лондон
+Пример вывода:
+Абрамовичи */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Модернизация ПО
+*/
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<String> list = new ArrayList<>();
+        while (true) {
+            String family = reader.readLine();
+            if (family.isEmpty()) {
+                break;
+            }
+
+            list.add(family);
+        }
+
+        // Read the house number
+        int houseNumber = Integer.parseInt(reader.readLine());
+
+        if (0 <= houseNumber && houseNumber < list.size()) {
+            String familyName = list.get(houseNumber);
+            System.out.println(familyName);
+        }
+    }
+}
