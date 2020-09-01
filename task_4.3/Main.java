5f80e30aa7fc2866506e88a225f339af59b6ca5a
MaximFeoktistov <69854595+MaximFeoktistov@users.noreply.github.com>
2020-09-02 00:47:17 +0300

    Update Main.java

--- task_4.3/Main.java
+++ task_4.3/Main.java
@@ -6,10 +6,47 @@
 Создать объект пегаса и вызвать метод fly();
 */
 
-/* Ещё делаеться */
+/* Ответ */
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-      //Ваш код
+        Horse agate = new Horse("Агат", "Англо-норманн", 200, 210, 60);
+        agate.run();
+        Pegasus stitch = new Pegasus("Стич", "Беркут",50,45,120);
+        stitch.fly();
     }
 }
+
+abstract class Animal {
+    String name;
+    String breed;
+    int height;
+    int weight;
+    int speed;
+    public Animal(String name, String breed, int height, int weight, int speed) {
+        this.name = name;
+        this.breed = breed;
+        this.height = height;
+        this.weight = weight;
+        this.speed = speed;
+    }
+}
+class Horse extends Animal {
+    public Horse(String name, String breed, int height, int weight, int speed) {
+        super(name, breed, height, weight, speed);
+    }
+    public void run() {
+        System.out.println("Игого, я поскакал(а)");
+    }
+}
+
+class Pegasus extends Horse {
+    public Pegasus(String name, String breed, int height, int weight, int speed) {
+        super(name, breed, height, weight, speed);
+    }
+    public void fly() {
+        System.out.println("Игого, я полетел(а)");
+    }
+}
+
+
