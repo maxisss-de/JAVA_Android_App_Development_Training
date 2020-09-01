27714d112dce94540653b169f16910667d067651
MaximFeoktistov <69854595+MaximFeoktistov@users.noreply.github.com>
2020-09-01 23:15:17 +0300

    Update Main.java

--- task_4.2/Main.java
+++ task_4.2/Main.java
@@ -17,24 +17,58 @@ Mouse jerryMouse = new Mouse("Jerry", 12 , 5),
 •	Должен быть создан хотя бы один объект типа Cat.
 */
 
-/* Ещё делаеться */
+/* Ответ */
 
 
 public class Main {
     public static void main(String[] args) {
-        Mouse jerryMouse = new Mouse("Jerry", 12, 5);
-
-        //напишите тут ваш код
+        Dog himalayasDog = new Dog("Гималай",35,7);
+        Dog micropDog = new Dog("Микроп",60,3);
+        Cat democratCat = new Cat("Демократ" ,12,25);
+        Cat einsteinCat = new Cat("Эйнштейн" ,15,26);
+        Cat chattererCat = new Cat("Говорун",10,24);
+        Mouse schultzMouse = new Mouse("Шульц", 2, 4);
+        Mouse al_caponeMouse = new Mouse("Аль Капоне", 2, 4);
+        Mouse sueMouse = new Mouse("Сью", 1, 3);
+        System.out.println("Кастинг на мультик Том и Джерри.");
+        System.out.println("Претенденты на роли:");
+        System.out.println("Привет! Меня зовут: "+himalayasDog.name+". Мой вес: "+himalayasDog.weight+" кг. Длина моего хвоста: "+himalayasDog.tail_size+" см.");
+        System.out.println("Привет! Меня зовут: "+micropDog.name+". Мой вес: "+micropDog.weight+" кг. Длина моего хвоста: "+micropDog.tail_size+" см.");
+        System.out.println("Привет! Меня зовут: "+democratCat.name+". Мой вес: "+democratCat.weight+" кг. Длина моего хвоста: "+democratCat.tail_size+" см.");
+        System.out.println("Привет! Меня зовут: "+einsteinCat.name+". Мой вес: "+einsteinCat.weight+" кг. Длина моего хвоста: "+einsteinCat.tail_size+" см.");
+        System.out.println("Привет! Меня зовут: "+chattererCat.name+". Мой вес: "+chattererCat.weight+" кг. Длина моего хвоста: "+chattererCat.tail_size+" см.");
+        System.out.println("Привет! Меня зовут: "+schultzMouse.name+". Мой вес: "+schultzMouse.weight+" кг. Длина моего хвоста: "+schultzMouse.tail_size+" см.");
+        System.out.println("Привет! Меня зовут: "+al_caponeMouse.name+". Мой вес: "+al_caponeMouse.weight+" кг. Длина моего хвоста: "+al_caponeMouse.tail_size+" см.");
+        System.out.println("Привет! Меня зовут: "+sueMouse.name+". Мой вес: "+sueMouse.weight+" кг. Длина моего хвоста: "+sueMouse.tail_size+" см.");
+    }
+}
+class Dog {
+    String name;
+    int weight;
+    int tail_size;
+    public Dog(String name, int weight, int tail_size) {
+        this.name = name;
+        this.weight = weight;
+        this.tail_size = tail_size;
+    }
+}
+class Cat {
+    String name;
+    int weight;
+    int tail_size;
+    public Cat(String name, int weight, int tail_size) {
+        this.name = name;
+        this.weight = weight;
+        this.tail_size = tail_size;
     }
 }
 class Mouse {
     String name;
-    int height;
-    int tail;
-
-    public Mouse(String name, int height, int tail) {
+    int weight;
+    int tail_size;
+    public Mouse(String name, int weight, int tail_size) {
         this.name = name;
-        this.height = height;
-        this.tail = tail;
+        this.weight = weight;
+        this.tail_size = tail_size;
     }
 }
