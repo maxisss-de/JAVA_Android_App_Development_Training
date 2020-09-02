98911ae34ee3d101dc3253257c3279df86e7dd25
MaximFeoktistov <69854595+MaximFeoktistov@users.noreply.github.com>
2020-09-02 18:00:04 +0300

    Update Main.java

--- task_5.2/Main.java
+++ task_5.2/Main.java
@@ -20,8 +20,29 @@
 
 /* Делаю  */
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        System.out.print ("Введите значение");
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        int e = scanner.nextInt();
+        int f = scanner.nextInt();
+        int g = scanner.nextInt();
+        int h = scanner.nextInt();
+        int i = scanner.nextInt();
+        System.out.println ("Вы ввели "+b+c+d+e+f+g+h+i+" значение");
+        int a = ;
+        if((a%2)==0){
+                System.out.println("Число "+b+c+d+e+f+g+h+i+" четное ");
+            }
+        else{
+            if((a%1)==0){
+                System.out.println("Число "+b+c+d+e+f+g+h+i+" нечетное ");
+            }
+        }
     }
 }
