757469bddbf85d49e02b218a58e3265696606147
MaximFeoktistov <69854595+MaximFeoktistov@users.noreply.github.com>
2020-09-03 04:28:04 +0300

    Update Main.java

--- task_5.2/Main.java
+++ task_5.2/Main.java
@@ -20,29 +20,55 @@
 
 /* Делаю  */
 
-import java.util.Scanner;
+import java.io.FileReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print ("Введите значение");
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        int d = scanner.nextInt();
-        int e = scanner.nextInt();
-        int f = scanner.nextInt();
-        int g = scanner.nextInt();
-        int h = scanner.nextInt();
-        int i = scanner.nextInt();
-        System.out.println ("Вы ввели "+b+c+d+e+f+g+h+i+" значение");
-        int a = ; /* Подумать....  */
-        if((a%2)==0){
-                System.out.println("Число "+b+c+d+e+f+g+h+i+" четное ");
-            }
-        else{
-            if((a%1)==0){
-                System.out.println("Число "+b+c+d+e+f+g+h+i+" нечетное ");
-            }
+    public static void main(String[] args) throws Exception {
+        FileReader reader = new FileReader("C:/Program Files/java/test.txt");
+        int c;
+        String document = "";
+        while ((c=reader.read())!=-1){
+            document += (char)c;
+        }
+        System.out.println("Вы выбрали числа:");
+        System.out.println(document);
+        
+        int i = Integer.parseInt(document.trim());
+
+
+        if((i%2)==0){
+            System.out.println("Числа "+i+" чётные ");
         }
     }
 }
+
+
+
+
+
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print ("Введите значение");
+//         int b = scanner.nextInt();
+//         int c = scanner.nextInt();
+//         int d = scanner.nextInt();
+//         int e = scanner.nextInt();
+//         int f = scanner.nextInt();
+//         int g = scanner.nextInt();
+//         int h = scanner.nextInt();
+//         int i = scanner.nextInt();
+//         System.out.println ("Вы ввели "+b+c+d+e+f+g+h+i+" значение");
+//         int a = ; /* Подумать....  */
+//         if((a%2)==0){
+//                 System.out.println("Число "+b+c+d+e+f+g+h+i+" четное ");
+//             }
+//         else{
+//             if((a%1)==0){
+//                 System.out.println("Число "+b+c+d+e+f+g+h+i+" нечетное ");
+//             }
+//         }
+//     }
+// }
