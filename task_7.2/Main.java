5c443386c2cdcd8070b870d46a0be26d7c4ae344
MaximFeoktistov <69854595+MaximFeoktistov@users.noreply.github.com>
2020-09-03 20:57:28 +0300

    Update and rename Server.java to Main.java

---
+++ task_7.2/Main.java
@@ -0,0 +1,36 @@
+/* 
+Задание: сделать чат с графическим интерфейсом
+Придумать дизайн для своего чата
+Видео Вебинара 03.09.2020
+
+JavaFX
+подключение
+смотреть с 1:03 до конца
+*/
+
+/* В разработке */
+
+
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+ public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
