fb88afcbb984331342a023508ae5dc2333a1d672
MaximFeoktistov <69854595+MaximFeoktistov@users.noreply.github.com>
2020-09-08 01:20:27 +0300

    Update and rename ProxyCheckerN1.java to ProxyChecker.java

--- task_6.1/ProxyChecker.java
+++ task_6.1/ProxyChecker.java
@@ -5,16 +5,17 @@
 /* В разработке */
 
 
-package com.company;
-
 import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.*;
 import java.util.Arrays;
 
-class ProxyCheckerN1 {
+public class ProxyChecker {
     public static void main(String[] args) throws Exception {
+        System.out.println("Программа сканирования списка адресов Прокси-серверов на предмет их доступности.");
+        System.out.println("Данные по адресам прокси-серверов будут считываться с файла C:/Program Files/java/ip.txt");
+        System.out.println("Читаем файл с адресами и запускаем опрос Прокси-серверов.");
         FileReader reader = new FileReader("C:/Program Files/java/ip.txt");
         int c;
         String ipList = "";
@@ -26,44 +27,37 @@ class ProxyCheckerN1 {
             String[] splitString = ipListArray[i].split(":");
             String ip = splitString[0].trim();
             int port = Integer.parseInt(splitString[1]);
-            MyThread t1 = new MyThread(ip,port);
+            Thread t1 = new Thread(new MyRunnableClass(ip,port,i));
             t1.start();
         }
     }
 }
 
-
-class MyThread extends Thread{
+class MyRunnableClass implements Runnable{
     String ip;
     int port;
-
-    public MyThread(String ip, int port) {
+    int i;
+    public MyRunnableClass(String ip, int port, int i) {
         this.ip = ip;
         this.port = port;
+        this.i = i;
     }
-
     @Override
     public void run(){
         Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ip, port));
-
         try {
             URLConnection connection = new URL("https://vozhzhaev.ru/test.php").openConnection(proxy);
-
             InputStream is = connection.getInputStream();
             InputStreamReader reader = new InputStreamReader(is);
             char[] buffer = new char[256];
             int rc;
-
             StringBuilder sb = new StringBuilder();
-
             while ((rc = reader.read(buffer)) != -1)
                 sb.append(buffer, 0, rc);
-
             reader.close();
-
-            System.out.println(getName()+"- ip: "+ip+":"+port+" - ДОСТУПЕН");
+            System.out.println("Поток - "+i+" - ip: "+ip+":"+port+" - РАБОТАЕТ");
         }catch (Exception e){
-            System.out.println(getName()+"- ip: "+ip+":"+port+" - недоступен");
+            System.out.println("Поток - "+i+" - ip: "+ip+":"+port+" - не работает");
         }
     }
 }
