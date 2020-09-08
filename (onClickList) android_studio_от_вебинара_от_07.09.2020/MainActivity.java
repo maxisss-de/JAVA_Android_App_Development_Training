3fe72f6908fe020aef90d7126217dd150481f41f
MaximFeoktistov <69854595+MaximFeoktistov@users.noreply.github.com>
2020-09-08 22:23:33 +0300

    Update MainActivity.java

--- (onClickList) android_studio_от_вебинара_от_07.09.2020/MainActivity.java
+++ (onClickList) android_studio_от_вебинара_от_07.09.2020/MainActivity.java
@@ -1,7 +1,8 @@
 /* Задание: Сделать кнопку "НЕТ", чтобы на экране показывалось уведомление неправильно*/
 
 
-/* Ответ */
+/* Ответ (Исправил) */
+
 
 package com.example.myapplication;
 
@@ -22,20 +23,64 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        trueBtn = findViewById(R.id.trueBtn); /поиск индификатора id кнопки ДА/
-        falseBtn = findViewById(R.id.falseBtn); /поиск индификатора id кнопки НЕТ/
+        trueBtn = findViewById(R.id.trueBtn);
+        falseBtn = findViewById(R.id.falseBtn);
 
         trueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Правильно", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, R.string.correct_toast,Toast.LENGTH_SHORT).show();
             }
         });
         falseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               Toast.makeText(MainActivity.this, "Неправильно", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
             }
         });
     }
 }
+
+
+
+
+
+
+
+
+// package com.example.myapplication;
+
+// import androidx.appcompat.app.AppCompatActivity;
+
+// import android.os.Bundle;
+// import android.view.View;
+// import android.widget.Button;
+// import android.widget.Toast;
+
+// public class MainActivity extends AppCompatActivity {
+
+//     private Button trueBtn; /* кнопка ДА */
+//     private Button falseBtn; /* кнопка НЕТ */
+
+//     @Override
+//     protected void onCreate(Bundle savedInstanceState) {
+//         super.onCreate(savedInstanceState);
+//         setContentView(R.layout.activity_main);
+
+//         trueBtn = findViewById(R.id.trueBtn); /поиск индификатора id кнопки ДА/
+//         falseBtn = findViewById(R.id.falseBtn); /поиск индификатора id кнопки НЕТ/
+
+//         trueBtn.setOnClickListener(new View.OnClickListener() {
+//             @Override
+//             public void onClick(View view) {
+//                 Toast.makeText(MainActivity.this, "Правильно", Toast.LENGTH_SHORT).show();
+//             }
+//         });
+//         falseBtn.setOnClickListener(new View.OnClickListener() {
+//             @Override
+//             public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Неправильно", Toast.LENGTH_SHORT).show();
+//             }
+//         });
+//     }
+// }
