d98f4586a19a7586aa53d0c0818fbf1dc9c51358
MaximFeoktistov <69854595+MaximFeoktistov@users.noreply.github.com>
2020-09-24 03:25:34 +0300

    Create Main.java

---
+++ Примеры работ/task_2.2/Main.java
@@ -0,0 +1,34 @@
+/*
+Написать функцию, которая вычисляет минимум из четырёх чисел.
+Функция min(a,b,c,d) должна использовать (вызывать) функцию min(a,b)
+Подсказка:
+Нужно написать тело двух существующих функций min.
+Требования:
+•	Программа должна выводить текст на экран.
+•	Методы min не должны выводить текст на экран.
+•	Метод main должен вызывать метод min(a, b) и метод min(a, b, c, d).
+•	Метод main должен выводить на экран результат работы методов min. Каждый раз с новой строки.
+•	Метод min(a, b) должен возвращать минимальное значение из чисел a, b.
+•	Метод min(a, b, c, d) должен использовать метод min(a, b)
+•	Метод min(a, b, c, d) должен возвращать минимальное значение из чисел a, b, c, d.
+*/
+
+public class Main {
+    public static int min(int a, int b, int c, int d) {
+        if ( min(a, b) <= min(c, d) )  return min(a, b);
+        return min(c, d);
+    }
+
+    public static int min(int a, int b) {
+        if ( a<=b ) return a;
+        return b;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(min(-20, -10));
+        System.out.println(min(-40, -10, -30, 40));
+        System.out.println(min(-20, -40, -30, 40));
+        System.out.println(min(-20, -10, -40, 40));
+        System.out.println(min(-20, -10, -30, -40));
+    }
+}
