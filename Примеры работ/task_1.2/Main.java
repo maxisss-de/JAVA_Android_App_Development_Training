50081b9dcd8297605c34060d279ae57f9e427b4f
MaximFeoktistov <69854595+MaximFeoktistov@users.noreply.github.com>
2020-09-24 03:17:32 +0300

    Create Main.java

---
+++ Примеры работ/task_1.2/Main.java
@@ -0,0 +1,24 @@
+/*
+Закомментируй ненужные строки кода, чтобы на экран вывелась надпись:
+2 плюс 3 равно 5
+Примечание: комментировать строки с объявлением переменных нельзя.
+*/
+// Пример комментария
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+
+ //       System.out.print("два");
+        System.out.print(b);
+        System.out.print(" плюс ");
+ //       System.out.print(" минус ");
+        System.out.print(a);
+ //       System.out.print("три");
+        System.out.print(" равно ");
+ //       System.out.print(" будет ");
+ //       System.out.print("пять");
+        System.out.print(a + b);
+    }
+}
