9a80326f60221198bdbc38d2d3641e4182c640f0
Maxim Wilhelm <69854595+Maxim-Wilhelm@users.noreply.github.com>
2022-11-07 00:52:32 +0300

    Update MainActivity.java

--- (onClickList) android_studio_от_вебинара_от_07.09.2020/MainActivity.java
+++ (onClickList) android_studio_от_вебинара_от_07.09.2020/MainActivity.java
@@ -1,8 +1,6 @@
+// ------------------------------------------------------------------------
 /* Задание: Сделать кнопку "НЕТ", чтобы на экране показывалось уведомление неправильно*/
-
-
-/* Ответ (Исправил) */
-
+// ------------------------------------------------------------------------
 
 package com.example.myapplication;
 
@@ -15,8 +13,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button trueBtn; /* кнопка ДА */
-    private Button falseBtn; /* кнопка НЕТ */
+    private Button trueBtn;         // кнопка ДА
+    private Button falseBtn;        // кнопка НЕТ
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,47 +38,3 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
-
-
-
-
-
-
-
-
-// package com.example.myapplication;
-
-// import androidx.appcompat.app.AppCompatActivity;
-
-// import android.os.Bundle;
-// import android.view.View;
-// import android.widget.Button;
-// import android.widget.Toast;
-
-// public class MainActivity extends AppCompatActivity {
-
-//     private Button trueBtn; /* кнопка ДА */
-//     private Button falseBtn; /* кнопка НЕТ */
-
-//     @Override
-//     protected void onCreate(Bundle savedInstanceState) {
-//         super.onCreate(savedInstanceState);
-//         setContentView(R.layout.activity_main);
-
-//         trueBtn = findViewById(R.id.trueBtn); /поиск индификатора id кнопки ДА/
-//         falseBtn = findViewById(R.id.falseBtn); /поиск индификатора id кнопки НЕТ/
-
-//         trueBtn.setOnClickListener(new View.OnClickListener() {
-//             @Override
-//             public void onClick(View view) {
-//                 Toast.makeText(MainActivity.this, "Правильно", Toast.LENGTH_SHORT).show();
-//             }
-//         });
-//         falseBtn.setOnClickListener(new View.OnClickListener() {
-//             @Override
-//             public void onClick(View view) {
-//                Toast.makeText(MainActivity.this, "Неправильно", Toast.LENGTH_SHORT).show();
-//             }
-//         });
-//     }
-// }
