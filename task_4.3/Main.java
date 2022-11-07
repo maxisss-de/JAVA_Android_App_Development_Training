9a563d12041af39ab42bba1364385e54af778ad3
Maxim Wilhelm <69854595+Maxim-Wilhelm@users.noreply.github.com>
2022-11-07 20:39:38 +0300

    Update Main.java

--- task_4.3/Main.java
+++ task_4.3/Main.java
@@ -1,14 +1,17 @@
+*/
+// ------------------------------------------------------------------------
 /*
+Задача:
 Создать абстрактный класс Animal с характеристиками животного.
 Создать класс Horse который наследуется от Animal, в классе Horse реализовать метод public void run(){ System.out.println("Игого, я поскакал(а)"); }
 Создать класс Pegasus который наследуется от Horse, в классе Pegasus реализовать метод public void fly(){ System.out.println("Игого, я полетел(а)"); }
 Создать объект лошади и вызвать метод run();
 Создать объект пегаса и вызвать метод fly();
 */
-
-/* Ответ */
+// ------------------------------------------------------------------------
 
 public class Main {
+    
     public static void main(String[] args) {
         Horse agate = new Horse("Агат", "Англо-норманн", 200, 210, 60);
         agate.run();
@@ -23,6 +26,7 @@ abstract class Animal {
     int height;
     int weight;
     int speed;
+    
     public Animal(String name, String breed, int height, int weight, int speed) {
         this.name = name;
         this.breed = breed;
@@ -31,22 +35,25 @@ abstract class Animal {
         this.speed = speed;
     }
 }
+
 class Horse extends Animal {
+    
     public Horse(String name, String breed, int height, int weight, int speed) {
         super(name, breed, height, weight, speed);
     }
+    
     public void run() {
         System.out.println("Игого, я поскакал(а)");
     }
 }
 
 class Pegasus extends Horse {
+    
     public Pegasus(String name, String breed, int height, int weight, int speed) {
         super(name, breed, height, weight, speed);
     }
+    
     public void fly() {
         System.out.println("Игого, я полетел(а)");
     }
 }
-
-
