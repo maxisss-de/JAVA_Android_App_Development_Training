799ec9895ecdc0c20e0ff645eaac26c0bda89343
Maxim Wilhelm <69854595+Maxim-Wilhelm@users.noreply.github.com>
2022-11-07 21:31:31 +0300

    Update Main.java

--- task_7.2/Main.java
+++ task_7.2/Main.java
@@ -1,25 +1,22 @@
+// ------------------------------------------------------------------------
 /* 
-Задание: сделать чат с графическим интерфейсом
+Задание: 
+Сделать чат с графическим интерфейсом
 Придумать дизайн для своего чата
-Видео Вебинара 03.09.2020
+(Видео Вебинара 03.09.2020 смотреть с 01:03 до конца)
 JavaFX
 подключение
-Cмотреть с 1:03 до конца
 */
-
-/* Ответ */
-
+// ------------------------------------------------------------------------
 
 package sample;
 
 import javafx.application.Application;
+import javafx.scene.control.TextArea;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.control.TextArea;
-
-
 
 public class Main extends Application {
 
@@ -29,9 +26,7 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 400, 275));
         primaryStage.show();
-
     }
-
 
     public static void main(String[] args) {
         launch(args);
