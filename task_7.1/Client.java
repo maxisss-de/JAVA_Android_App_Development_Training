cecaf1fd7e103cc5fedea3797cc55090aa3b6020
Maxim Wilhelm <69854595+Maxim-Wilhelm@users.noreply.github.com>
2022-11-07 21:26:39 +0300

    Update Client.java

--- task_7.1/Client.java
+++ task_7.1/Client.java
@@ -1,17 +1,18 @@
+// ------------------------------------------------------------------------
 /* 
-Задание: сделать чат
+Задание: 
+Сделать чат
 */
+// ------------------------------------------------------------------------
 
-
-/* Ответ */
-
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
-import java.net.Socket;
 import java.util.Scanner;
+import java.net.Socket;
 
 public class Client {
+    
     public static void main(String[] args) {
         try {
             Socket socket = new Socket("localhost",8190);
@@ -19,6 +20,7 @@ public class Client {
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             Scanner scanner = new Scanner(System.in);
             Thread responseThread = new Thread(new Runnable() {
+               
                 @Override
                 public void run() {
                     while (true){
