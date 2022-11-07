b481f33d36839e808ab640ccc6cd124695726732
Maxim Wilhelm <69854595+Maxim-Wilhelm@users.noreply.github.com>
2022-11-07 21:17:38 +0300

    Update Main.java

--- task_5.1/Main.java
+++ task_5.1/Main.java
@@ -17,9 +17,9 @@
 */
 // ------------------------------------------------------------------------
 
+import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
